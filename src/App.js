2ada402e9bfaf140decdfd144edb15e2296564a5
DevUnusual <carlos.meneses.g.s@gmail.com>
2020-10-28 13:36:34 -0300

    terceiro desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from "react";
+import React,{useState, useeffect} from 'react';
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setProjects] = useState([])
+
+  useeffect(() => {
+    api.get('/repositories').then((response) => {
+      setProjects(response.data)
+    })
+  },[])
+
   async function handleAddRepository() {
-    // TODO
+    const newRepository = {
+      url: "https://github.com/Rocketseat/unform",
+      title: `Repositorio${(repositories.length + 1)}`,
+      techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+    }
+
+    api.post('repositories', newRepository)
+
+    setProjects([...repositories, newRepository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
         <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+          {repositories.map(repository => <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
-          </button>
+            </button>
+          </li>)}
         </li>
       </ul>
 
